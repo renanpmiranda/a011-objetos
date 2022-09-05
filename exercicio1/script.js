552e720346fefb7561830f5c89dd8d54916e2218
renanpmiranda <renanpmiranda5@gmail.com>
2022-09-05 13:21:12 -0300

    a011_exercicio1

---
+++ exercicio1/script.js
@@ -0,0 +1,22 @@
+const sacolao = []
+
+const objeto1 = {
+    nome: 'banana',
+    preco: 10,
+    disponibilidade: true
+}
+
+const objeto2 = {
+    nome: 'mamão',
+    preco: 18,
+    disponibilidade: true
+}
+
+const objeto3 = {
+    nome: 'maracujá',
+    preco: 22,
+    disponibilidade: false
+}
+
+sacolao.push(objeto1, objeto2, objeto3)
+console.log(sacolao)
